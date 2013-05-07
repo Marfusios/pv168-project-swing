8a3928d513a82e5ab882cf357b4c77920a1e29d8
Roman Jakubik <romanxxyz@seznam.cz>
2013-05-07 13:54:50 +0200

    +menu

--- src/main/java/pv168/project/swing/MainWindow.java
+++ src/main/java/pv168/project/swing/MainWindow.java
@@ -3,6 +3,8 @@ package pv168.project.swing;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created with IntelliJ IDEA.
@@ -31,26 +33,64 @@ public class MainWindow {
             }
         });  */
     }
-
-
-
-
-
-
-
-
-
+    private JMenuBar createMenu() {
+        //hlavní úroveň menu
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+        final JMenu helpMenu = new JMenu("Help");
+        JMenu lafMenu = new JMenu("Look and feel");
+        menuBar.add(fileMenu);
+        menuBar.add(lafMenu);
+        menuBar.add(helpMenu);
+        menuBar.add(Box.createHorizontalGlue());
+        //menu File
+        JMenuItem exitMenuItem = new JMenuItem("Exit");
+        fileMenu.add(exitMenuItem);
+        exitMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(1);
+            }
+        });
+        //menu Help
+        JMenuItem aboutMenuItem = new JMenuItem("About");
+        helpMenu.add(aboutMenuItem);
+        aboutMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(helpMenu,"Skvělá aplikace (c) Já","About",JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+        for (final UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+            JMenuItem item = new JMenuItem(info.getName());
+            lafMenu.add(item);
+            item.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent ev) {
+                    try {
+                        UIManager.setLookAndFeel(info.getClassName());
+                        SwingUtilities.updateComponentTreeUI(MainWindow.this.topPanel);
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            });
+        }
+        return menuBar;
+    }
 
     public static void main(String[] args) {
+        final MainWindow  thisWindow = new MainWindow();
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
                 JFrame frame = new JFrame("Home evidency");
                 frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-                frame.setContentPane(new MainWindow().topPanel);
+                frame.setContentPane(thisWindow.topPanel);
                 frame.setPreferredSize(new Dimension(800,600));
                 frame.pack();
                 frame.setVisible(true);
+                frame.setJMenuBar(thisWindow.createMenu());
             }
         });
     }
