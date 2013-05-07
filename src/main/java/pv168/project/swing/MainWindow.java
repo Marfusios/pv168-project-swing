0ef470b1ed10ecad0bfa75a01da8daf5f1f8edb3
Marfusios <m@mkotas.cz>
2013-05-07 12:44:24 +0200

    And finally

---
+++ src/main/java/pv168/project/swing/MainWindow.java
@@ -0,0 +1,57 @@
+package pv168.project.swing;
+
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: m4r10
+ * Date: 5/7/13
+ * Time: 10:49 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MainWindow {
+
+    private JPanel topPanel;
+    private JTabbedPane tabbedPane1;
+    private JPanel tabBooks;
+    private JPanel tabDisks;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+
+    public MainWindow() {
+        /*button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("tlačítko zmáčknuto");
+            }
+        });  */
+    }
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frame = new JFrame("Home evidency");
+                frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+                frame.setContentPane(new MainWindow().topPanel);
+                frame.setPreferredSize(new Dimension(800,600));
+                frame.pack();
+                frame.setVisible(true);
+            }
+        });
+    }
+
+
+}
